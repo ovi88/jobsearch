851eaa5ef9efb2abf1c345bcfc37e27cb3cc1e44
Dmitry Ovechkin <dmitry.ovechkin@mocomedia.ru>
2016-03-06 21:44:46 +0300

    start search from hh

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,13 @@
 class JobsController < ApplicationController
+  require 'open-uri'
   def index
     @job_name = params[:job_name]
-    puts @job_name
+    #puts @job_name
+    result_str = open("https://api.hh.ru/vacancies?text=#{@job_name}&area=2&vacancy_search_fields=name&period=7").read
+    #puts responce
+    result_json = JSON.parse(result_str)
+    @result_array = result_json['items']
+    #puts result_array[0]
+
   end
 end
