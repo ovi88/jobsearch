3a156a1cd0e62d8d759dd157a066672c78c74a83
Dmitry Ovechkin <dmitry.ovechkin@mocomedia.ru>
2016-03-20 20:47:12 +0300

    add functional for calculate time in mashine to work

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,14 @@
 class JobsController < ApplicationController
   require 'open-uri'
+  require 'dotenv'
+
   def index
+
     @job_name = params[:job_name]
-    #puts @job_name
     @hh_result = []
-    hh_api = open("https://api.hh.ru/vacancies?text=#{@job_name}&area=2&vacancy_search_fields=name&period=30").read
-    #puts responce
+    home = "59.7458,30.0795"
+    Dotenv.load
+    hh_api = open("https://api.hh.ru/vacancies?text=#{@job_name}&area=2&vacancy_search_fields=name&period=14").read
     hh_json = JSON.parse(hh_api)
     hh_array = hh_json['items']
     hh_array.each do |h|
@@ -31,9 +34,19 @@ class JobsController < ApplicationController
         hh_dop_url_json = JSON.parse(hh_dop_url_result)
         hh_hash['experience'] = hh_dop_url_json['experience']['name']
         unless hh_dop_url_json.dig('address', 'metro_stations') == nil
+          lat = hh_dop_url_json['address']['lat']
+          lng = hh_dop_url_json['address']['lng']
+          next_monday = Date.today.next_week.advance(:days=>0)
+          next_monday = next_monday.to_time + 9.hours
+          departure_time = next_monday.to_i
+          road_dur = open("https://maps.googleapis.com/maps/api/distancematrix/json?departure_time=#{departure_time}&traffic_model=pessimistic&origins=#{home}&destinations=#{lat},#{lng}&key=#{ENV["GOOGLE_MAP_KEY"]}").read
+          road_json = JSON.parse(road_dur)
+          hh_hash['duration'] = road_json['rows'][0]['elements'][0]['duration_in_traffic']['text']
+          #puts vacancy
+          #puts road_json
           unless hh_dop_url_json['address']['metro_stations'][0] == nil
             hh_hash['metro'] = hh_dop_url_json['address']['metro_stations'][0]['station_name']
-          end  
+          end
         end
         #hh_hash['description'] = hh_dop_url_json['description']
         unless hh_dop_url_json['key_skills'].empty?
@@ -47,6 +60,6 @@ class JobsController < ApplicationController
     #puts @hh_result
     end
     #puts result_array[0]
-
   end
+
 end
